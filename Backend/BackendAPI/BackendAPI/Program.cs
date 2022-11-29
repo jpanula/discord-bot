3fd29f1dc3f7243bdbd4e00c44f813aa495b60dc
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-29 13:13:53 +0200

    Add DbContext to builder.Services

--- Backend/BackendAPI/BackendAPI/Program.cs
+++ Backend/BackendAPI/BackendAPI/Program.cs
@@ -1,8 +1,13 @@
+using BackendAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration["Database:ConnectionString"];
 
 // Add services to the container.
-
+builder.Services.AddDbContext<BotDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers().AddNewtonsoftJson();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
