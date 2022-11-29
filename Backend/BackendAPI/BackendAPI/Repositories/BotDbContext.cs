a2aadc1bdb4b7e2bec4f75f441ffa2b5326bdd19
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-29 13:00:45 +0200

    Create DbContext for the bot's database

---
+++ Backend/BackendAPI/BackendAPI/Repositories/BotDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAPI.Repositories
+{
+    public class BotDbContext : DbContext
+    {
+        public DbSet<Magic8BallResponse> Magic8BallResponses{ get; set; }
+
+        public BotDbContext(DbContextOptions<BotDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Magic8BallResponse>().ToTable("Magic8BallResponse");
+        }
+    }
+}
