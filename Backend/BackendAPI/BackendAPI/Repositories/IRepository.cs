8e986ad93c0d5235305df2313f9bb1edbba0e5b7
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-30 15:08:02 +0200

    Add GetLatest to IRepository interface

--- Backend/BackendAPI/BackendAPI/Repositories/IRepository.cs
+++ Backend/BackendAPI/BackendAPI/Repositories/IRepository.cs
@@ -4,6 +4,7 @@
     {
         public List<T> Get();
         public T GetById(int id);
+        public T GetLatest();
         public void Add(T item);
         public void Update(int id, T item);
         public void Delete(int id);
