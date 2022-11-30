a2960874ce316dd818f2aaddbd4e6aa21f9bfd00
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-30 11:55:04 +0200

    Create generic repository interface

---
+++ Backend/BackendAPI/BackendAPI/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BackendAPI.Repositories
+{
+    public interface IRepository<T>
+    {
+        public List<T> Get();
+        public T GetById(int id);
+        public void Add(T item);
+        public void Update(int id, T item);
+        public void Delete(int id);
+    }
+}
