42334d5cc226adc6ea973c0d9cc7c646f1cba9cd
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-30 13:05:10 +0200

    Create Magic8BallResponse repository

---
+++ Backend/BackendAPI/BackendAPI/Repositories/Magic8BallRepository.cs
@@ -0,0 +1,21 @@
+﻿using BackendAPI.Models;
+
+namespace BackendAPI.Repositories
+{
+    public class Magic8BallRepository : RepoBase<Magic8BallResponse>, IMagic8BallRepository
+    {
+        public Magic8BallRepository(BotDbContext context) : base(context)
+        {
+        }
+
+        public override List<Magic8BallResponse> Get()
+        {
+            return _context.Magic8BallResponses.ToList();
+        }
+
+        public override Magic8BallResponse GetById(int id)
+        {
+            return _context.Magic8BallResponses.FirstOrDefault(item => item.Id == id);
+        }
+    }
+}
