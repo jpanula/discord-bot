d47aa641d5ebc0f5505e8c4e6d437c7c9113b552
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-30 15:47:34 +0200

    Add Magic8BallController

---
+++ Backend/BackendAPI/BackendAPI/Controllers/Magic8BallController.cs
@@ -0,0 +1,44 @@
+﻿using BackendAPI.Models;
+using BackendAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Magic8BallController : ControllerBase
+    {
+        private IMagic8BallService _service;
+
+        public Magic8BallController(IMagic8BallService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string content)
+        {
+            Magic8BallResponse newResponse = _service.Add(content);
+            return Created(Request.GetEncodedUrl() + "/" + newResponse.Id, newResponse);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var response = _service.Delete(id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+    }
+}
