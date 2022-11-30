095568df5049c5aa8ae15950af58ac532c0f5c89
Jaakko Panula <panula.jaakko@gmail.com>
2022-11-30 14:54:03 +0200

    Create interface for Magic8ballservice

---
+++ Backend/BackendAPI/BackendAPI/Services/IMagic8BallService.cs
@@ -0,0 +1,11 @@
+﻿using BackendAPI.Models;
+
+namespace BackendAPI.Services
+{
+    public interface IMagic8BallService
+    {
+        public List<Magic8BallResponse> Get();
+        public Magic8BallResponse Add(string response);
+        public bool Delete(int id);
+    }
+}
