ea124ba95deb7148570f4d4487a59bddaf092710
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-01 11:28:01 +0200

    Create project for Discord Bot and Add NuGet Package for Discord.Net

---
+++ Bot/DiscordBot/DiscordBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+
+namespace DiscordBot
+{
+    internal class Program
+    {
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        static void Main(string[] args) => new Program().MainAsync();
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
