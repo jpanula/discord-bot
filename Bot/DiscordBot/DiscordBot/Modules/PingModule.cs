f513c1318a45b77d4bd3ceb6fa2c007fb3982113
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-02 15:52:23 +0200

    Restructure code for more modularity WIP

---
+++ Bot/DiscordBot/DiscordBot/Modules/PingModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    internal class PingModule : InteractionModuleBase
+    {
+        [SlashCommand("ping", "Pings the bot")]
+        public async Task Ping() => await RespondAsync("pong");
+    }
+}
--- Bot/DiscordBot/DiscordBot/Program.cs
+++ Bot/DiscordBot/DiscordBot/Program.cs
@@ -1,69 +1,90 @@
 ﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
 using Discord.Net;
 using Discord.WebSocket;
+using DiscordBot.Modules;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 namespace DiscordBot
 {
     internal class Program
     {
-        private DiscordSocketClient _client;
+        public static Task Main(string[] args) => new Program().MainAsync();
 
-        private Task Log(LogMessage msg)
+        private readonly DiscordSocketClient _client;
+
+        private readonly InteractionService _interactions;
+        private readonly IServiceProvider _services;
+
+        private Program()
         {
-            Console.WriteLine(msg.ToString());
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+            });
+
+            _interactions = new InteractionService(_client, new InteractionServiceConfig
+            {
+                LogLevel = LogSeverity.Info,
+            });
+
+            _client.Log += Log;
+            _interactions.Log += Log;
+
+            _services = ConfigureServices();
+        }
+        private static IServiceProvider ConfigureServices()
+        {
+            // Setup DI container here
+            var map = new ServiceCollection();
+            return map.BuildServiceProvider();
+        }
+
+        private static Task Log(LogMessage message)
+        {
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
+            Console.ResetColor();
+
             return Task.CompletedTask;
         }
 
-        public async Task CreateCommands()
-        {
-            var globalPingCommand = new SlashCommandBuilder();
-            globalPingCommand.WithName("ping");
-            globalPingCommand.WithDescription("Pings the bot");
-
-            try
-            {
-                await _client.CreateGlobalApplicationCommandAsync(globalPingCommand.Build());
-            }
-            catch (ApplicationCommandException exception)
-            {
-                await Log(new LogMessage(LogSeverity.Error, "CreateCommands", JsonConvert.SerializeObject(exception.Errors, Formatting.Indented), exception));
-            }
-        }
-
-        private async Task SlashCommandHandler(SocketSlashCommand command)
-        {
-            switch (command.Data.Name)
-            {
-                case "ping":
-                    command.RespondAsync("pong");
-                    break;
-                default:
-                    command.RespondAsync("Unrecognized command");
-                    break;
-            }
-        }
-
-        public static Task Main(string[] args) => new Program().MainAsync();
         public async Task MainAsync()
         {
             var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
-            _client.Ready += CreateCommands;
-            _client.SlashCommandExecuted += SlashCommandHandler;
+            await InitCommands();
 
             var token = configuration["BotToken"];
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
+            await Task.Delay(Timeout.Infinite);
+        }
 
-
-
-            await Task.Delay(-1);
+        private async Task InitCommands()
+        {
+            await _interactions.AddModuleAsync<PingModule>(_services);
         }
     }
 }
