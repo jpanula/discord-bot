c5780a05c38db45494761d388c367722dbedac1d
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-07 16:17:59 +0200

    Change EventVote DiscordUserId column to plural

--- Backend/BackendAPI/BackendAPI/Migrations/20221207141616_ChangeDiscordUserIdToPlural.Designer.cs
+++ Backend/BackendAPI/BackendAPI/Migrations/20221207141616_ChangeDiscordUserIdToPlural.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using BackendAPI.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BackendAPI.Migrations
 {
     [DbContext(typeof(BotDbContext))]
-    partial class BotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221207141616_ChangeDiscordUserIdToPlural")]
+    partial class ChangeDiscordUserIdToPlural
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/BackendAPI/BackendAPI/Migrations/20221207141616_ChangeDiscordUserIdToPlural.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDiscordUserIdToPlural : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiscordUserId",
+                table: "EventVote",
+                newName: "DiscordUserIds");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "EventVote",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiscordUserIds",
+                table: "EventVote",
+                newName: "DiscordUserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "EventVote",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
