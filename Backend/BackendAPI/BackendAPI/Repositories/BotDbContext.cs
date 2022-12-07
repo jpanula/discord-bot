a1a667e2a413f8067e691543f052b8a1d97caeff
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-07 14:23:48 +0200

    Add event and event vote to OnModelCreating

--- Backend/BackendAPI/BackendAPI/Repositories/BotDbContext.cs
+++ Backend/BackendAPI/BackendAPI/Repositories/BotDbContext.cs
@@ -6,12 +6,16 @@ namespace BackendAPI.Repositories
     public class BotDbContext : DbContext
     {
         public DbSet<Magic8BallResponse> Magic8BallResponses{ get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventVote> EventVotes { get; set; }
 
         public BotDbContext(DbContextOptions<BotDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Magic8BallResponse>().ToTable("Magic8BallResponse");
+            modelBuilder.Entity<Event>().ToTable("Event");
+            modelBuilder.Entity<EventVote>().ToTable("EventVote");
         }
     }
 }
