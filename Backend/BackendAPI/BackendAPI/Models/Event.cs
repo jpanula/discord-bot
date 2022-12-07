a6f1790a120b696a0d4148850d8e14e6f899b4dd
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-07 14:20:53 +0200

    Add models for event and event votes

---
+++ Backend/BackendAPI/BackendAPI/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendAPI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public ICollection<EventVote> Votes { get; set; }
+    }
+}
---
+++ Backend/BackendAPI/BackendAPI/Models/EventVote.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendAPI.Models
+{
+    public class EventVote
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public string Emoji { get; set; }
+        [Required]
+        public List<string> DiscordUserId { get; set; }
+    }
+}
