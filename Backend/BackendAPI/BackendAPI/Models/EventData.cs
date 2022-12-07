b42843bcb0435192e6e439f656aa5ec52d9f7937
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-07 14:39:59 +0200

    Add data models for Event and EventVote

---
+++ Backend/BackendAPI/BackendAPI/Models/EventData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendAPI.Models
+{
+    public class EventData
+    {
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Backend/BackendAPI/BackendAPI/Models/EventVoteData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendAPI.Models
+{
+    public class EventVoteData
+    {
+        
+        public string? Name { get; set; }
+        [Required]
+        public string Emoji { get; set; }
+        [Required]
+        public List<string> DiscordUserId { get; set; }
+    }
+}
