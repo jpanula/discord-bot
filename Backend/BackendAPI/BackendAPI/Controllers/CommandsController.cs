be2f0cc33ab56539cff3b36ac7bdc2328fe0856d
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-14 15:44:38 +0200

    Add CommandsController for command api endpoints

---
+++ Backend/BackendAPI/BackendAPI/Controllers/CommandsController.cs
@@ -0,0 +1,46 @@
+﻿using BackendAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandService _commandService;
+
+        public CommandsController(ICommandService commandService)
+        {
+            _commandService = commandService;
+        }
+
+        [HttpGet("CommandGroups")]
+        public IActionResult GetCommandGroups()
+        {
+            return Ok(_commandService.GetCommandGroups());
+        }
+
+        [HttpGet("CommandGroups/{id}")]
+        public IActionResult GetCommandGroupById(int id)
+        {
+            var commandGroup = _commandService.GetCommandGroupById(id);
+            if (commandGroup == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandGroup);
+        }
+
+        [HttpGet("SubCommands/{id}")]
+        public IActionResult GetSubCommandById(int id)
+        {
+            var subCommand = _commandService.GetSubCommandById(id);
+            if (subCommand == null)
+            {
+                return NotFound();
+            }
+            return Ok(subCommand);
+        }
+    }
+}
