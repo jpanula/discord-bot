00bec092f7e1fd25ac58a54b7327916e23863afb
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-14 15:44:16 +0200

    Add DI for Command repos and service

--- Backend/BackendAPI/BackendAPI/Program.cs
+++ Backend/BackendAPI/BackendAPI/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddScoped<IMagic8BallService, Magic8BallService>();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventVoteRepository, EventVoteRepository>();
 builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<ICommandGroupRepository, CommandGroupRepository>();
+builder.Services.AddScoped<ISubCommandRepository, SubCommandRepository>();
+builder.Services.AddScoped<ICommandParameterRepository, CommandParameterRepository>();
+builder.Services.AddScoped<ICommandService, CommandService>();
 builder.Services.AddSingleton<Random>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
