31af3f674db3a5774072db3d474c7f5158160d9c
Jaakko Panula <panula.jaakko@gmail.com>
2022-12-15 15:38:44 +0200

    Add CommandList component to App

--- Frontend/bot-frontend/src/App.tsx
+++ Frontend/bot-frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import CommandList from "./Components/CommandList";
 
 function App() {
   return (
     <div className="App">
+      <CommandList/>
     </div>
   );
 }
